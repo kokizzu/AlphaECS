71b1f6aa1ab2419130053e520e0db2c6bcb42d04
Tim Riley <tim@finergames.com>
2017-05-08 12:52:44 +0800

    - pulled in most of the recent changes that lee has made to ecsrx

--- Assets/Plugins/AlphaECS/Core/Extensions/IDisposableExtensions.cs
+++ Assets/Plugins/AlphaECS/Core/Extensions/IDisposableExtensions.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace AlphaECS.Extensions
+namespace AlphaECS
 {
-    public static class IDisposableExtensions
-    {
-        public static void DisposeAll(this IEnumerable<IDisposable> disposables)
-        {	
-//			disposables.ForEachRun(x => x.Dispose());
-		}
-    }
+	public static class IDisposableExtensions
+	{
+		public static void DisposeAll(this IEnumerable<IDisposable> disposables)
+		{ disposables.ForEachRun(x => x.Dispose()); }
+
+//		public static void DisposeAll(this IEnumerable<SubscriptionToken> disposables)
+//		{ disposables.ForEachRun(x => x.Disposable.Dispose()); }
+
+		public static void AddTo(this IDisposable currentDisposable, ICollection<IDisposable> disposables)
+		{ disposables.Add(currentDisposable); }
+	}
 }
