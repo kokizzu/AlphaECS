147b79cd8d6063b52fb3db8deb21fa3230308c92
tim <tim@finergames.com>
2017-05-09 12:32:20 +0800

    - added OnAdd and OnRemove extension methods to simplifying group sub setup - started adding back in notion of predicate filtering for groups - finished adding first pass of predicate logic - fixed bug with cached entities - started adding notion of cached groups, but have not implemented for now as it raises a larger question of “who owns the group”, i.e., what and how to dispose of shared groups. normally groups should be disposed along with the system they were created in… we can do this now in the system where after a group is created, we do “this.Disposer.Add(group)” -> when the system disposes, the group will too. this really needs to be cleaned up…

--- Assets/Plugins/AlphaECS/Core/Groups/Group.cs
+++ Assets/Plugins/AlphaECS/Core/Groups/Group.cs
@@ -15,6 +15,8 @@ namespace AlphaECS
 
 		public string Name { get; set; }
 
+		public ReactiveCollection<IEntity> cachedEntities = new ReactiveCollection<IEntity>();
+
 		ReactiveCollection<IEntity> _entities = new ReactiveCollection<IEntity>();
 		public ReactiveCollection<IEntity> Entities
 		{
@@ -25,6 +27,8 @@ namespace AlphaECS
 		public IEnumerable<Type> Components { get; set; }
 		public Predicate<IEntity> Predicate { get; set; }
 
+		private List<Func<IEntity, ReactiveProperty<bool>>> Predicates = new List<Func<IEntity, ReactiveProperty<bool>>> ();
+
 		protected CompositeDisposable _disposer = new CompositeDisposable();
 		public CompositeDisposable Disposer
 		{
@@ -32,17 +36,15 @@ namespace AlphaECS
 			set { _disposer = value; }
 		}
 
-		public Group(params Type[] components)
+		public Group(Type[] components, List<Func<IEntity, ReactiveProperty<bool>>> predicates)
         {
 			Components = components;
-			Predicate = null;
-        }
 
-//		public Group(Predicate<IEntity> predicate, params Type[] components)
-//        {
-//			Components = components;
-//			Predicate = predicate;
-//        }
+			foreach (var predicate in predicates)
+			{
+				Predicates.Add (predicate);
+			}
+        }
 
 		[Inject]
 		public void Setup(IEventSystem eventSystem, IPoolManager poolManager)
@@ -50,33 +52,61 @@ namespace AlphaECS
 			EventSystem = eventSystem;
 			EntityPool = poolManager.GetPool ();
 
+			cachedEntities.ObserveAdd ().Select (x => x.Value).Subscribe (entity =>
+			{
+				if(Predicates.Count == 0)
+				{
+					PreAdd(entity);
+					AddEntity(entity);
+					return;
+				}
+				
+				var bools = new List<ReactiveProperty<bool>>();
+				foreach (var predicate in Predicates)
+				{
+					bools.Add(predicate.Invoke(entity));
+				}
+				var onLatest = Observable.CombineLatest(bools.ToArray());
+				onLatest.DistinctUntilChanged().Subscribe(values =>
+				{
+					if(values.All(value => value == true))
+					{
+						PreAdd(entity);
+						AddEntity(entity);
+					}
+					else
+					{
+						PreRemove(entity);
+						RemoveEntity(entity);
+					}
+				}).AddTo (this.Disposer);;
+			}).AddTo (this.Disposer);
+
+			cachedEntities.ObserveRemove ().Select (x => x.Value).Subscribe (entity =>
+			{
+				PreRemove(entity);
+				RemoveEntity(entity);
+			}).AddTo (this.Disposer);
+
 			foreach (IEntity entity in EntityPool.Entities)
 			{
 				if (entity.HasComponents (Components.ToArray ()))
 				{
-					AddEntity (entity);
+					cachedEntities.Add(entity);
 				}
 			}
 
-			EventSystem.OnEvent<EntityAddedEvent> ().Where (_ => _.Entity.HasComponents (Components.ToArray())).Subscribe (_ =>
-			{
-				AddEntity(_.Entity);
-			}).AddTo(this);
+			EventSystem.OnEvent<EntityAddedEvent> ().Where (_ => _.Entity.HasComponents (Components.ToArray()) && cachedEntities.Contains(_.Entity) == false).Subscribe (_ =>
+			{ cachedEntities.Add(_.Entity); }).AddTo(this);
 
-			EventSystem.OnEvent<EntityRemovedEvent> ().Where (_ => Entities.Contains(_.Entity)).Subscribe (_ =>
-			{
-				RemoveEntity(_.Entity);
-			}).AddTo(this);
+			EventSystem.OnEvent<EntityRemovedEvent> ().Where (_ => cachedEntities.Contains(_.Entity)).Subscribe (_ =>
+			{ cachedEntities.Remove(_.Entity); }).AddTo(this);
 
-			EventSystem.OnEvent<ComponentAddedEvent> ().Where (_ => _.Entity.HasComponents (Components.ToArray()) && Entities.Contains(_.Entity) == false).Subscribe (_ =>
-			{
-				AddEntity(_.Entity);
-			}).AddTo(this);
+			EventSystem.OnEvent<ComponentAddedEvent> ().Where (_ => _.Entity.HasComponents (Components.ToArray()) && cachedEntities.Contains(_.Entity) == false).Subscribe (_ =>
+			{ cachedEntities.Add(_.Entity); }).AddTo(this);
 
-			EventSystem.OnEvent<ComponentRemovedEvent> ().Where (_ => Components.Contains(_.Component.GetType()) && Entities.Contains(_.Entity)).Subscribe (_ =>
-			{
-				RemoveEntity(_.Entity);
-			}).AddTo(this);
+			EventSystem.OnEvent<ComponentRemovedEvent> ().Where (_ => Components.Contains(_.Component.GetType()) && cachedEntities.Contains(_.Entity)).Subscribe (_ =>
+			{ cachedEntities.Remove(_.Entity); }).AddTo(this);
 		}
 
 		void AddEntity(IEntity entity)
@@ -93,5 +123,8 @@ namespace AlphaECS
 		{
 			Disposer.Dispose ();
 		}
+
+		protected virtual void PreAdd(IEntity entity) {}
+		protected virtual void PreRemove(IEntity entity) {}
     }
 }
--- Assets/Plugins/AlphaECS/Core/Groups/GroupFactory.cs
+++ Assets/Plugins/AlphaECS/Core/Groups/GroupFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using AlphaECS;
 using Zenject;
 using UniRx;
+using System.Linq;
 
 namespace AlphaECS
 {
@@ -10,49 +11,49 @@ namespace AlphaECS
 	public class GroupFactory
     {
 		[Inject] protected DiContainer Container = null;
+//		protected Dictionary<HashSet<Type>, Group> Groups = new Dictionary<HashSet<Type>, Group>();
 
-		public Group Create(Type[] types)
+		private Type[] types;
+		private List<Func<IEntity, ReactiveProperty<bool>>> predicates = new List<Func<IEntity, ReactiveProperty<bool>>> ();
+
+		public Group Create(Type[] _types)
 		{
-			var group = new Group (types);
+			this.types = _types;
+			return this.Create ();
+		}
+
+		public Group Create()
+		{
+//			var hashSet = new HashSet<Type> (types);
+//			foreach (var key in Groups.Keys)
+//			{
+//				if (hashSet.SetEquals(key) && predicates.Count == 0)
+//				{
+//					this.types = null;
+//					this.predicates.Clear();
+//					return Groups [key];
+//				}
+//			}
+				
+			var group = new Group (types, predicates);
 			Container.Inject (group);
+
+			types = null;
+			predicates.Clear();
+//			Groups.Add (hashSet, group);
 			return group;
 		}
-//		[Inject] public IEventSystem EventSystem { get; set; }
-//        private List<Type> _components;
-//        private Predicate<IEntity> _predicate;
-//
-//        public ReactiveGroupBuilder()
-//        {
-//            _components = new List<Type>();
-//        }
-//
-//		public ReactiveGroupBuilder Create()
-//        {
-//            _components = new List<Type>();
-//            return this;
-//        }
-//
-//		public ReactiveGroupBuilder WithComponent<T>() where T : class, IComponent
-//        {
-//            _components.Add(typeof(T));
-//            return this;
-//        }
-//
-//		public ReactiveGroupBuilder WithPredicate(Predicate<IEntity> predicate)
-//        {
-//            _predicate = predicate;
-//            return this;
-//        }
-//
-//		public static IReactiveGroup Build(List<Type> components)
-//        {
-////			var group = new ReactiveGroup(_predicate, _components.ToArray());
-//			var group = new ReactiveGroup(components.ToArray());
-//			EventSystem.OnEvent<ComponentAddedEvent> ().Where (e => components.Contains (e.Component.GetType ())).Subscribe (_ =>
-//			{
-//			
-//			});
-//			return group;
-//		}
+
+		public GroupFactory AddTypes(Type[] _types)
+		{
+			this.types = _types;
+			return this;
+		}
+
+		public GroupFactory WithPredicate(Func<IEntity, ReactiveProperty<bool>> predicate)
+		{
+			this.predicates.Add (predicate);
+			return this;
+		}
     }
 }
